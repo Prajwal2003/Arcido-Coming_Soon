ee9b13ffb8bb733a517bc33cd426c36d89793cce
Prajwal2003 <prajwalgh13@gamil.com>
2025-04-30 15:37:50 +0530

    Coming Soon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/Users/starkz/Desktop/Work/Arcido/arcido/src/app/arcido.png" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
